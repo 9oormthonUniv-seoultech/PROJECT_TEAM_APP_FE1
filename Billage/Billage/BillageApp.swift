afb03b7206be407e558991e1c626871c734738df
Sangwoo Byeon <tkddn0518@naver.com>
2024-09-10 19:42:02 +0900

    [Setting] #1 - 프로젝트 생성

---
+++ Billage/Billage/BillageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BillageApp.swift
+//  Billage
+//
+//  Created by 변상우 on 9/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BillageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
