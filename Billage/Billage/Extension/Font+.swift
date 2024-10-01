1e5238c2521408b3254ada184c16587e9668b716
Sangwoo Byeon <tkddn0518@naver.com>
2024-10-01 21:38:49 +0900

    [Setting] 폰트 추가

---
+++ Billage/Billage/Extension/Font+.swift
@@ -0,0 +1,26 @@
+//
+//  Font+.swift
+//  Billage
+//
+//  Created by 변상우 on 9/21/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Font {
+    
+    static let logo: Font = .custom("Pretendard-ExtraBold", size: 20)
+    static let head: Font = .custom("Pretendard-SemiBold", size: 20)
+    static let head2: Font = .custom("Pretendard-SemiBold", size: 18)
+    static let bodybold: Font = .custom("Pretendard-Bold", size: 16)
+    static let bodysemibold: Font = .custom("Pretendard-SemiBold", size: 16)
+    static let body: Font = .custom("Pretendard-Regular", size: 16)
+    static let option: Font = .custom("Pretendard-Regular", size: 16)
+    static let par: Font = .custom("Pretendard-Regular", size: 14)
+    static let sub: Font = .custom("Pretendard-Regular", size: 12)
+    static let times: Font = .custom("Pretendard-SemiBold", size: 10)
+    static let calendar: Font = .custom("Pretendard-Medium", size: 16)
+    static let navigationbar: Font = .custom("Pretendard-Regular", size: 10)
+    
+}
